69cc677a709e03900dbe0b04fff64e49822e9be3
syedauraain <160678874+syedauraain@users.noreply.github.com>
2024-04-18 15:13:47 -0700

    Did famous_Quote

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let answer = "A person who never made a mistake never tried anything";
+let author = "Albert_Einstein";
+console.log(`${author} once said, ${answer}`);
---
+++ app.ts
@@ -0,0 +1,3 @@
+let answer:string="A person who never made a mistake never tried anything";
+let author:string="Albert_Einstein";
+console.log(`${author} once said, ${answer}`);
